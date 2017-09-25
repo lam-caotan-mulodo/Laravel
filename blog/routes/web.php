ccfab29c75ddf61ce199fa5e3d92016b357dce68
lam-caotan-mulodo <lam.caotan.mulodo@gmail.com>
2017-09-25 16:15:19 +0700

    ep7

---
+++ blog/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Task;
+
+Route::get('/tasks', function () {
+
+	$tasks = Task::all();
+
+    return view('tasks.index', compact('tasks'));
+});
+
+
+
+Route::get('tasks/{task}', function ($id) {
+
+	$task = Task::find($id);
+
+    return view('tasks.show', compact('task'));
+});
+
