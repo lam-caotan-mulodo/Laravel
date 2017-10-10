f82253fd33193c40422c8e56d585351756776561
lam-caotan-mulodo <lam.caotan.mulodo@gmail.com>
2017-10-10 17:30:40 +0700

    ep14

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -2,24 +2,25 @@
 
 @section ('content')
 	<div class="col-sm-8 blog-main">
-		<ul class="list-group"
-		<h1>{{ $post->title }}</h1>
+		<ul class="list-group">
+			<h1>{{ $post->title }}</h1>
 
-		{{ $post->body }}
+			{{ $post->body }}
 
-		<hr>
+			<hr>
 
-		
-		<div class="comments">
-			@foreach ($post->comments as $comment)
-				<li class="list-group-item">
-					<strong>
-						{{ $comment->created_at->diffForHumans() }}: &nbsp;
-					</strong>
-					{{ $comment->body }}
-				</article>
-			@endforeach		
+			<div class="comments">
+				@foreach ($post->comments as $comment)
+					<li class="list-group-item">
+						<article>
+							<strong>
+								{{ $comment->created_at->diffForHumans() }}: &nbsp;
+							</strong>
+							{{ $comment->body }}
+						</article>
+					</li>
+				@endforeach		
+			</div>
 		</ul>
-		</div> 
-	</div>
+	</div> 
 @endsection
