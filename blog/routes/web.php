5c7113757a7b1e93084f1d688b8f2d42889478c1
lam-caotan-mulodo <lam.caotan.mulodo@gmail.com>
2017-10-11 14:18:55 +0700

    ep16

---
+++ blog/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+Route::get('/','PostsController@index');
+Route::post('/posts','PostsController@store');
+Route::get('/posts/create','PostsController@create');
+Route::get('/posts/{post}','PostsController@show');
+Route::post('/posts/{post}/comments','CommentsController@addComment');
